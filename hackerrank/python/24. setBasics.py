f30e45a16962f3f4a0e80caeb1a1a03eb6a04a61
hideyuki <automationhidey@gmail.com>
2020-10-28 16:41:04 +0800

    hackerrank new

---
+++ hackerrank/python/24. setBasics.py
@@ -0,0 +1,13 @@
+
+def main():
+    _ = input()
+    a = set(map(int, input().split()))
+    _ = input()
+    b = set(map(int, input().split()))
+    diffa, diffb = a.difference(b), b.difference(a)
+    diffa.update(diffb)
+    for v in sorted(list(diffa)):
+        print(v)
+
+if __name__ == "__main__":
+    main() 
