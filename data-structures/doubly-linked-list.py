f74478cae20d89d9f7c082558183c55225a6d990
hideyuki <automationhidey@gmail.com>
2020-12-17 11:23:56 +0800

    data structures

--- data-structures/doubly-linked-list.py
+++ data-structures/doubly-linked-list.py
@@ -3,21 +3,23 @@ class DoubleLinkedList:
         self.head = None
         self.tail = None
     
-    def insertFront(self, node):
+    def insertFront(self, value):
+        newNode = Node(value)
         if not self.tail:
-            self.tail = node
+            self.tail = newNode
         if self.head:
-            self.head.prev = node
-            node.next = self.head
-        self.head = node
+            self.head.prev = newNode
+            newNode.next = self.head
+        self.head = newNode
     
-    def insertBack(self, node):
+    def insertBack(self, value):
+        newNode = Node(value)
         if not self.head:
-            self.head = node
+            self.head = newNode
         if self.tail:
-            self.tail.next = node
-            node.prev = self.tail
-        self.tail = node
+            self.tail.next = newNode
+            newNode.prev = self.tail
+        self.tail = newNode
 
     def removeFront(self):
         if not self.head:
@@ -27,6 +29,25 @@ class DoubleLinkedList:
             return
         self.head = self.head.next
         self.head.prev = None
+
+    def removeBack(self):
+        if not self.tail:
+            return
+        if self.head == self.tail:
+            self.head, self.tail = None, None
+            return
+        self.tail = self.tail.prev
+        self.tail.next = None
+    
+    def insertAfter(self, prevNode, value):
+        if prevNode is None:
+            return
+        newNode = Node(value)
+        newNode.next = prevNode.next
+        newNode.prev = prevNode
+        prevNode.next = newNode
+        if newNode.next:
+            newNode.next.prev = newNode
 
     def printItems(self):
         auxilaryNode = self.head
@@ -44,9 +65,9 @@ class Node:
 def main():
     a = DoubleLinkedList()
     for i in range(5):
-        a.insertFront(Node(i))
+        a.insertFront(i)
     for j in range(5, 10):
-        a.insertBack(Node(j))
+        a.insertBack(j)
     for k in range(3):
         a.removeFront()
     a.printItems()
